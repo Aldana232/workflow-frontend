55d54be51221b1742a795977d8a02a3596f048b1
Aldana232 <orientecampeon43@gmail.com>
2026-04-27 23:40:17 -0400

    Fix missing wsCollabUrl in production environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.workflow-demo.site/api',
   wsUrl: 'wss://api.workflow-demo.site/ws',
+  wsCollabUrl: 'wss://api.workflow-demo.site/collab',
 };
